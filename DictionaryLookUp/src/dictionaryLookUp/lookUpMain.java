a12a2f3a7aa31fbec58c32cf51810d60b60fdded
Eric Bryan Klinginsmith <quinks@live.com>
2015-01-16 14:01:03 -0800

    Added read loop to processWord()

--- DictionaryLookUp/src/dictionaryLookUp/lookUpMain.java
+++ DictionaryLookUp/src/dictionaryLookUp/lookUpMain.java
@@ -1,9 +1,12 @@
 package dictionaryLookUp;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class lookUpMain {
 
@@ -13,7 +16,7 @@ public class lookUpMain {
 		Path path = null;
 		String pwd = "";
 		File dictFile = null;
-		if (len < 2) {
+		if (len < 1) {
 			try {
 	            pwd = new java.io.File(".").getCanonicalPath();
 			}
@@ -24,18 +27,19 @@ public class lookUpMain {
 			dictFile = path.toFile();
 		}
 		else {
-			dictFile = new File(args[1]);
+			dictFile = new File(args[0]);
 		}
 
-		if (dictFile.exists() && !dictFile.isDirectory()) {
+		if (dictFile.exists() && !dictFile.isDirectory() && dictFile.isFile() && dictFile.canRead()) {
 
 			String word = getWord();
-			processWord(dictFile);
+			System.out.println("Echo: " + word);
+			processWord(dictFile, word);
 
 		}
 		else {
 
-			System.err.println("Error: " + dictFile.getPath() + " does not exists or it is not a regular file");
+			System.err.println("Error: " + dictFile.getPath() + " does not exists or it is not a regular file or it cannont be read.");
 			System.exit(1);
 
 		}
@@ -43,11 +47,34 @@ public class lookUpMain {
 	}
 
 	public static String getWord() {
-		return "";
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = null;
+
+        System.out.print("Enter a word: ");
+	    try {
+	        word = br.readLine();
+	    }
+	    catch (IOException ioe) {
+	        System.err.println("getWord() IO error trying to read word, because:" + ioe.getLocalizedMessage());
+	        System.exit(1);
+	    }
+
+	    word = word.toUpperCase();
+		return word;
+
 	}
 
-	public static void processWord(File dictFile) {
-		
+	public static void processWord(File dictFile, String word) {
+
+		Scanner scanner = new Scanner(dictFile.getAbsolutePath());
+		while (scanner.hasNextLine()) {
+
+			String line = scanner.nextLine().trim();
+			System.out.print(line);
+
+		}
+
 	}
 
 }
